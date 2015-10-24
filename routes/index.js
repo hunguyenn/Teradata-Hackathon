2614b5b5c8ba14ce59e7608639a50e825adb411b
Hung Nguyen <hg.ngyn@gmail.com>
2015-10-23 20:58:57 -0700

    restructure home page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,7 +8,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/visualize', function(req, res, next) {
-  res.render('visualize');
+	// var urlz = 'http://dragon.teradata.ws:1080/tdrest/systems/xTD150/queries';
+
+ //    request.post({
+ //    	url: urlz,
+ //    	headers: {
+ //    		'Accept': "application/vnd.com.teradata.rest-v1.0+json",
+ //    		'Authorization': 'Basic ' + new Buffer('hack_user07:tdhackathon').toString('base64'),
+ //    		'Content-Type': 'application/json'
+ //    	},
+ //    	data: JSON.stringify({
+ //    		'query': 'select * from health.life_expectancy_hivaids_countries',
+ //    		// 'queryBands': {'applicationName': 'MyApp', 'version': '1.0'},
+ //    	})
+ //    	}, function(err, response, body) {
+ //    	if (err) {
+ //    		console.log(err);
+ //    		// console.log(response);
+ //    		throw err;
+ //    	}
+ //    	console.log(body);
+        res.render('visualize');
+    // });  
 });
 
 module.exports = router;
