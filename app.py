11839e5517f70dbfbc6d1bd6b71f198898e7fcfb
Antriksh Yadav <antrikshy@gmail.com>
2015-10-23 17:38:35 -0700

    Added blank index.html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('templates/index.html')
 
 @app.route('/visualize')
 def render_visualize():
