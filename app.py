4d3403d692ae5972db1a434163b2f2da1dec5cd0
Antriksh Yadav <antrikshy@gmail.com>
2015-10-23 17:34:56 -0700

    Added blank HTML file

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/visualize')
+def render_visualize():
+    return render_template('templates/visualize.html')
 
 if __name__ == '__main__':
     app.run()
